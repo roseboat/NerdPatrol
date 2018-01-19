bd30829ae716b4dfad895cb4cd94a6be940de601
William Kirrage <2354535k@student.gla.ac.uk>
2018-01-19 15:00:54 +0000

    database stuff - stuck

--- src/commandline/Database.java
+++ src/commandline/Database.java
@@ -3,6 +3,7 @@ package commandline;
 import java.sql.*;
 
 import javax.swing.JOptionPane;
+import javax.swing.table.DefaultTableModel;
 
 public class Database {
 
@@ -46,40 +47,10 @@ public class Database {
 		}
 	}
 
-	// method to upload game statistics, with parameters for:
-	// how many times the player won and each other computer player won, and ultimate
-	// winner
 	
-	//winner is just a string that needs to be consistently be player, or computer1, computer2 etc. it should probably reference something as a foreign key,
-	//but it doesn't at the moment...
-	
-	// ALSO: variable number of players... don't really want to make 4 separate
-	// methods depending on how many computer players are playing>>.....?
-	
-	
-	// test for varying arguments - doesn't work
-	public void gameStats(String winner, int numberRounds, int numberDraws, int winsPlayer, int... winsComputers) {
-		Statement stmt = null;
-		// manual auto-increment for game number
-		int gameNumber = gameNumber() + 1;
-		String query = "INSERT INTO toptrumps.gamestats VALUES (" + gameNumber + ", " + numberRounds + ", "
-				+ numberDraws + ", " + winsPlayer + ", " + winsComputers[0] + ", " + winsComputers[1] + ", "
-				+ winsComputers[2] + ", '" + winner + "')";
-
-		try {
-			stmt = connection.createStatement();
-			int rs = stmt.executeUpdate(query);
-			System.out.println(rs);
-		} catch (SQLException e) {
-			System.err.println("error executing query " + query);
-
-			JOptionPane.showMessageDialog(null, "Error - check data entered correctly");
-		}
-	}
-
 	// overloaded methods for variable number of players, there is probably a better
 	// way to do this.....
-	
+
 	// for 1 AI player
 	public void gameStats(String winner, int numberRounds, int numberDraws, int winsPlayer, int winsComputer1) {
 		Statement stmt = null;
@@ -182,13 +153,33 @@ public class Database {
 
 		return gameNumber;
 	}
-	
+
+	// method to get total computer wins
+	public int getDraws() {
+		Statement stmt = null;
+		int result = 0;
+		String query = "SELECT SUM (gamestats.numberdraws) AS target FROM toptrumps.gamestats";
+		try {
+			stmt = connection.createStatement();
+			ResultSet rs = stmt.executeQuery(query);
+
+			while (rs.next()) {
+				result = rs.getInt("target");
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+			System.err.println("error executing query " + query);
+		}
+		return result;
+	}
 	
 
 	// for testing
 	public static void main(String args[]) {
 		Database x = new Database();
-		x.gameStats("player", 30, 5, 20, 1,1); // two AI player test
+		//x.gameStats("player", 30, 5, 20, 1, 1); // two AI player test
+		System.out.println(x.getComputerWins());
+		System.out.println(x.getDraws());
 		// x.closeConnection();
 	}
 
